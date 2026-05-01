29cb20e197feb8c299806f4a2736bf1701d92e89
Seoneui <ellieella2031@gmail.com>
2026-05-01 16:02:03 +0900

    feat: basic button 구현

---
+++ lib/shared/widgets/basic_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:ongi_app/core/constants/constants.dart';
+
+const double _largeWidth = double.infinity;
+
+class BasicButton extends StatelessWidget {
+  final String text;
+  final bool isClickable;
+  final VoidCallback? onPressed;
+
+  const BasicButton({
+    super.key,
+    required this.text,
+    required this.isClickable,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: _largeWidth,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: isClickable ? onPressed : null,
+        style: ElevatedButton.styleFrom(
+          backgroundColor:
+              isClickable ? OngiColor.primary : OngiColor.systemGray02,
+          foregroundColor: isClickable ? OngiColor.white50 : OngiColor.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          textStyle: OngiTextStyle.button18,
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
