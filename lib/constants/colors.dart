1d21bf7fc09d5916352f5af9ae9df9430c414562
Seoneui <ellieella2031@gmail.com>
2026-05-01 15:10:08 +0900

    design: text, color 설정

---
+++ lib/constants/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final class OngiColor {
+  // Brand Color
+  static const primary = Color(0xFFFF752B);
+
+  static const systemGray05 = Color(0xFF000000);
+  static const systemGray04 = Color(0xFF626262);
+  static const systemGray03 = Color(0xFFAEAEB2);
+  static const systemGray02 = Color(0xFFEBEBF0);
+  static const systemGray01 = Color(0xFFF4F4F4);
+
+  static const white50 = Color(0xFFFFFFFF);
+
+  static const fail = Color(0xFFFF3B30);
+  static const success = Color(0xFF01A501);
+}
